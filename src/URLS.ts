29e241090ecdfcaef9b2a58cabaa7acee5c157d7
Gabe Camacho <gabestuf@gmail.com>
2023-03-20 16:34:03 -0400

    ad

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,3 +1,3 @@
 const build = true;
 
-export default build ? "https://gabestuf.com/inq" : "http://localhost:3000/inq";
+export default build ? "https://www.gabestuf.com/inq" : "http://localhost:3000/inq";
