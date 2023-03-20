e93abd8902dd71948c63e06932f97b6bbbb10394
gabestuf <gabestuf@gmail.com>
2023-03-20 09:31:21 -0400

    updated url

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const build = true;
-const url = build ? "https://gabestuf.com" : "http://localhost:3000";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/inq": url,
+      "/inq": "https://gabestuf.com",
     },
   },
 });
